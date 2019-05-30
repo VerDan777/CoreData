6d922707dc5e0f1cee7b48089a8d6af79d5896ec
Danil Vereschagin <d.vereschagin@wellyes.ru>
2019-05-30 09:53:56 +0300

    - Setup ImagePickerController

--- CoreData/CreateCompanyViewController.swift
+++ CoreData/CreateCompanyViewController.swift
@@ -13,7 +13,7 @@ protocol CreateCompanyDelegate {
     func didAddCompany(company: Company)
 }
 
-class CreateCompanyViewController: UIViewController {
+class CreateCompanyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var delegate: CompaniesViewController?
     
@@ -26,9 +26,15 @@ class CreateCompanyViewController: UIViewController {
         return nameLabel;
     }();
     
-    let companyImageView: UIImageView = {
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "select_photo_empty"));
+    let companyImageView: UIButton = {
+        let imageView = UIButton(type: .system);
+//        imageView.isUserInteractionEnabled = true;
+        imageView.setImage(#imageLiteral(resourceName: "select_photo_empty").withRenderingMode(.alwaysOriginal), for: .normal);
         imageView.translatesAutoresizingMaskIntoConstraints = false;
+        imageView.layer.masksToBounds = true;
+        imageView.layer.cornerRadius = 50;
+//        imageView.layer.
+        imageView.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
         return imageView;
     }();
     
@@ -39,7 +45,6 @@ class CreateCompanyViewController: UIViewController {
         textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
         textfield.leftViewMode = UITextField.ViewMode.always;
         textfield.translatesAutoresizingMaskIntoConstraints = false;
-//        textfield.frame = CGRect(x: 0, y: 0, width: 414, height: 100);
         return textfield;
     }();
     
@@ -57,46 +62,58 @@ class CreateCompanyViewController: UIViewController {
         return vw;
     }();
 
+    @objc func handleSelectPhoto() {
+        print("try...");
+        let imagePickerController = UIImagePickerController();
+        imagePickerController.allowsEditing = true;
+        imagePickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate;
+        
+        present(imagePickerController, animated: true, completion: nil);
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(whiteView);
+        
+        self.view.addSubview(companyImageView);
+        
         view.backgroundColor = .tealColor;
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap));
-        self.view.addGestureRecognizer(tap);
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap));
+//        self.view.addGestureRecognizer(tap);
         
-        self.whiteView.addSubview(tf);
-        self.whiteView.addSubview(nameLb);
-        
+//        self.whiteView.addSubview(tf);
         navigationItem.title = "Create Company";
         
-        self.whiteView.addSubview(companyImageView);
+        self.view.addSubview(whiteView);
         
-        whiteView.topAnchor.constraint(equalTo: self.nameLb.bottomAnchor).isActive = true;
+        
+        whiteView.topAnchor.constraint(equalTo: self.companyImageView.bottomAnchor, constant: 8).isActive = true;
         whiteView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true;
         whiteView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true;
         whiteView.heightAnchor.constraint(equalToConstant: 350).isActive = true;
         
-        companyImageView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true;
+        self.whiteView.addSubview(nameLb);
+
+        companyImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8).isActive = true;
         companyImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true;
         companyImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true;
         companyImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true;
         
-        nameLb.topAnchor.constraint(equalTo: self.companyImageView.bottomAnchor).isActive = true;
+        nameLb.topAnchor.constraint(equalTo: self.whiteView.topAnchor).isActive = true;
         nameLb.leftAnchor.constraint(equalTo: self.whiteView.leftAnchor).isActive = true;
-        nameLb.heightAnchor.constraint(equalTo: tf.heightAnchor).isActive = true;
+        nameLb.heightAnchor.constraint(equalToConstant: 50).isActive = true;
         nameLb.widthAnchor.constraint(equalToConstant: 100).isActive = true;
+//
+        self.whiteView.addSubview(tf);
         
-        tf.topAnchor.constraint(equalTo: self.companyImageView.bottomAnchor).isActive = true;
-        tf.leftAnchor.constraint(equalTo: self.nameLb.rightAnchor).isActive = true;
+        tf.topAnchor.constraint(equalTo: self.whiteView.topAnchor).isActive = true;
+        tf.leftAnchor.constraint(equalTo: self.nameLb.rightAnchor, constant: 8).isActive = true;
         tf.rightAnchor.constraint(equalTo: self.whiteView.rightAnchor).isActive = true;
         tf.heightAnchor.constraint(equalToConstant: 50).isActive = true;
-        
+//
         self.whiteView.addSubview(datePicker);
-        
-        
-        datePicker.topAnchor.constraint(equalTo: self.whiteView.topAnchor).isActive = true;
+//
+        datePicker.topAnchor.constraint(equalTo: self.nameLb.bottomAnchor, constant: 8).isActive = true;
         datePicker.leftAnchor.constraint(equalTo: self.whiteView.leftAnchor).isActive = true;
         datePicker.rightAnchor.constraint(equalTo: self.whiteView.rightAnchor).isActive = true;
         datePicker.centerYAnchor.constraint(equalTo: self.whiteView.centerYAnchor).isActive = true;
@@ -107,6 +124,25 @@ class CreateCompanyViewController: UIViewController {
         self.setupButtons();
         
         // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(info);
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            self.companyImageView.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal);
+        } else {
+          if let originalImage = info[.originalImage] as? UIImage {
+            self.companyImageView.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal);
+        }
+    }
+        
+        self.dismiss(animated: true, completion: nil);
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil);
     }
     
     
